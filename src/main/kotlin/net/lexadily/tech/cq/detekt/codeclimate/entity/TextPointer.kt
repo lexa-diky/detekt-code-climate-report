c311cc1141e0f521b75fe06c31324a38633d6c8a
Aleksey <alexey.yakovlef@gmail.com>
2023-12-22 01:05:12 +0700

    fixed wrong visibility

--- src/main/kotlin/net/lexadily/tech/cq/detekt/codeclimate/entity/TextPointer.kt
+++ src/main/kotlin/net/lexadily/tech/cq/detekt/codeclimate/entity/TextPointer.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class TextPointer(
+internal data class TextPointer(
     @SerialName("line")
     val line: Int,
     @SerialName("column")
